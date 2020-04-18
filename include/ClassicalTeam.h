376d55f47cec246069c83b322ebb3a6cd2578544
Jeremy (Xiaoyi) Cai <xyc@mit.edu>
2020-04-17 20:55:09 -0400

    add function declaration for traj planning

--- include/ClassicalTeam.h
+++ include/ClassicalTeam.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <Eigen/Core>
+#include <vector>
 #include "SimState.h"
 
 class ClassicalTeam
 {
+    void planTraj(const Eigen::Vector4d &x0, const Eigen::Vector4d &xT, const double &dt, const int &T, std::vector<Eigen::Vector2d> &vels);
 public:
     ClassicalTeam();
     ClassicalTeam(int team);
